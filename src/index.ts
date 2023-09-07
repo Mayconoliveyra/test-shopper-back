a66bd201847d26af9238936be63a65668711f131
Maycon Oliveira <mayconbrito1998@hotmail.com>
2023-09-07 11:23:31 -0300

    feat: Criado arquivo de configuração de variável

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { server } from "./server";
 
-server.listen(3030, () => {
-  console.log(`App rodando na porta ${3030}`);
+// Verifico se foi configurado a Porta que vai rodar a aplicação, caso contrario retorna mensagem de error.
+if (!process.env.PORT) {
+  console.error(
+    "Erro: Para iniciar a aplicação, a variável 'PORT' deve ser configurada no arquivo .env"
+  );
+  process.exit();
+}
+
+server.listen(process.env.PORT, () => {
+  console.log(`App rodando na porta ${process.env.PORT}`);
 });
