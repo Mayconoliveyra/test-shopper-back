6a92a50b259704869fc78df9da3da5bb07fb44a0
Maycon Oliveira <mayconbrito1998@hotmail.com>
2023-09-07 18:43:29 -0300

    feat: Adicionado novas variáveis  e alterado o nome da variável da porta que o servidor executa.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { server } from "./server";
 
 // Verifico se foi configurado a Porta que vai rodar a aplicação, caso contrario retorna mensagem de error.
-if (!process.env.PORT) {
+if (!process.env.SERVER_PORT) {
   console.error(
-    "Erro: Para iniciar a aplicação, a variável 'PORT' deve ser configurada no arquivo .env"
+    "Erro: Para iniciar a aplicação, a variável 'SERVER_PORT' deve ser configurada no arquivo .env"
   );
   process.exit();
 }
 
-server.listen(process.env.PORT, () => {
-  console.log(`App rodando na porta ${process.env.PORT}`);
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(`App rodando na porta ${process.env.SERVER_PORT}`);
 });
