fc46cfd6b04a072f143f23e768bdb06149a3a704
Maycon Oliveira <mayconbrito1998@hotmail.com>
2023-09-07 11:27:33 -0300

    refactor: Configurado arquivo de rotas

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,12 @@
+import "dotenv/config";
 import express from "express";
 
-import "dotenv/config";
-import { StatusCodes } from "http-status-codes";
+import { router } from "./routes";
 
 const server = express();
 
 server.use(express.json());
 
-server.get("/test", (req, res) => {
-  console.log("Testado com sucesso!");
-
-  return res.status(StatusCodes.OK).send("Testado com sucesso!");
-});
+server.use(router);
 
 export { server };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,1 +1,12 @@
-export {};
+import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const router = Router();
+
+router.get("/test", (req, res) => {
+  console.log("Testado com sucesso!");
+
+  return res.status(StatusCodes.OK).json("Testado com sucesso!");
+});
+
+export { router };
