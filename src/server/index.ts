8accce976030397dac61f7fe9f5de03317525080
Maycon Oliveira <mayconbrito1998@hotmail.com>
2023-09-07 16:38:20 -0300

    feat: Adicionado cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import { router } from "./routes";
 
 const server = express();
 
 server.use(express.json());
+
+server.use(cors());
 
 server.use(router);
 
