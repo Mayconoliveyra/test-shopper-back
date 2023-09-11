acaaa847d7e70f18602128efe16f24e6681236c0
Maycon Oliveira <mayconbrito1998@hotmail.com>
2023-09-11 15:59:25 -0300

    refactor: Criado algumas validações simples

--- src/server/controllers/priceManager/updatePrices.ts
+++ src/server/controllers/priceManager/updatePrices.ts
@@ -14,7 +14,38 @@ export const updatePricesValidation = async (
 ) => {
   const dataProducts = req.body.dataProducts;
 
-  if (!(dataProducts.length > 0)) {
+  // Faz uma validação básica, só para garantir que ta recebendo um array valido.
+  if (dataProducts && dataProducts.length > 0) {
+    for (const product of dataProducts) {
+      //  Não pode ter itens com erro
+      if ("msgError" in product) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          errors: {
+            default: "Não pode existir produto com regra quebrada.",
+          },
+        });
+      }
+      // 'code' e 'new_sales_price' deve ser numérico
+      else if (
+        typeof product.code !== "number" ||
+        typeof product.new_sales_price !== "number"
+      ) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          errors: {
+            default: "'code' é 'new_sales_price' deve ser numérico",
+          },
+        });
+      }
+      // Se o produto é pacote  'new_cost_price_pack' deve ta preenchido com valor numérico
+      else if ("new_cost_price_pack" in product) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          errors: {
+            default: "'new_cost_price_pack' deve ser numérico",
+          },
+        });
+      }
+    }
+  } else {
     return res.status(StatusCodes.BAD_REQUEST).json({
       errors: {
         default: "Deve ser informando o array de produtos.",
